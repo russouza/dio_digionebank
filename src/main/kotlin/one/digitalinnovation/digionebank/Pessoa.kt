7ad66b2095685d84aa0fb525cf1ae50da464b9ef
Rubens Souza <russouza@gmail.com>
2021-03-11 08:47:06 -0300

    Membros de uma classe, construtor padrão e função.

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -6,6 +6,10 @@ class Pessoa {
     var cpf: String = "196.196.106-80" // Seria alterável
     private set // Protege contra alterações.
 
+    constructor() //construtor padrão
+
+    fun pessoalInfo() = "$nome e $cpf"
+
     inner class Endereco {
         var rua: String = "Rua São Paulo"
         var nro: String =  "931"
@@ -21,4 +25,9 @@ fun main() {
     println(john.nome)
     println(john.cpf)
     println(john.Endereco().rua +", " + john.Endereco().nro)
+
+    println(john.pessoalInfo())
+
+    val rubens = Pessoa()
+    println(rubens.pessoalInfo())
 }
